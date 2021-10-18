34ae3f72b675c177ebe2d4e8888b0fd975db677c
Lucas Gabriel <lucasgsilva2102@gmail.com>
2021-10-18 09:04:09 -0300

    feat: create add product controller

---
+++ src/application/controllers/AddProduct.ts
@@ -0,0 +1,38 @@
+import { HttpRequest, HttpResponse, Controller } from '../protocols'
+import { AddProduct } from '../../core/usecases/AddProduct'
+import { MissingParamError } from '../errors'
+import { ok, badRequest, serverError } from '../helpers/http-helper'
+
+export class AddProductController implements Controller {
+  private readonly addProduct: AddProduct
+
+  constructor (addProduct: AddProduct) {
+    this.addProduct = addProduct
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const requiredFields = ['name', 'description', 'purchasePrice', 'salePrice', 'quantity']
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+
+      const { name, description, purchasePrice, salePrice, quantity } = httpRequest.body
+
+      const product = await this.addProduct.add({
+        name,
+        description,
+        purchasePrice,
+        salePrice,
+        quantity 
+      })
+
+      return ok(product)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
