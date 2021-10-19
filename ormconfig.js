736846b65d3b50829f6e8bd1d0800c911dc2d225
Lucas Gabriel <lucasgsilva2102@gmail.com>
2021-10-19 17:56:15 -0300

    chore: config migrations

--- ormconfig.js
+++ ormconfig.js
@@ -7,5 +7,9 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: ['src/infra/databases/postgres/entities/index.{js,ts}']
+  entities: ['src/infra/databases/postgres/entities/index.{js,ts}'],
+  migrations: ['src/infra/databases/postgres/migrations/*.{js,ts}'],
+  cli: {
+    migrationsDir: 'src/infra/databases/postgres/migrations'
+  }
 }
