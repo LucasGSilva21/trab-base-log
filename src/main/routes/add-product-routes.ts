4aa42f3002097bc339860378deb35d6b406d804e
Lucas Gabriel <lucasgsilva2102@gmail.com>
2021-10-19 19:32:51 -0300

    refactor: change name

--- src/main/routes/add-product-routes.ts
+++ src/main/routes/add-product-routes.ts
@@ -3,5 +3,5 @@ import { makeAddProductController } from '../factories/add-product'
 import { adaptRoute } from '../adapters/express-route-adapter'
 
 export default (router: Router): void => {
-  router.post('/product', adaptRoute(makeAddProductController()))
+  router.post('/products', adaptRoute(makeAddProductController()))
 }
