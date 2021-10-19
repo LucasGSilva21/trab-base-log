06a2e027122ee14dbf19f0bff680cddc15f064c1
Lucas Gabriel <lucasgsilva2102@gmail.com>
2021-10-19 18:25:48 -0300

    feat: create products migration

---
+++ src/infra/databases/postgres/migrations/1634676511507-CreateProducts.ts
@@ -0,0 +1,57 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateProducts1634676511507 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'description',
+            type: 'text',
+            isNullable: true
+          },
+          {
+            name: 'purchase_price',
+            type: 'decimal'
+          },
+          {
+            name: 'sale_price',
+            type: 'decimal'
+          },
+          {
+            name: 'quantity',
+            type: 'int'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products')
+  }
+}
