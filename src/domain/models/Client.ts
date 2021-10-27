1e28cc84cbdb16909ac34064adb3bd07123af269
Lucas Gabriel <lucasgsilva2102@gmail.com>
2021-10-27 14:45:51 -0300

    feat: create client model

---
+++ src/domain/models/Client.ts
@@ -0,0 +1,11 @@
+import { Gender } from '../protocols'
+
+export class ClientModel {
+  name: string
+  email: string
+  cpf: string
+  birthDate: Date
+  gender: Gender
+  phone: string
+  address: string
+}
