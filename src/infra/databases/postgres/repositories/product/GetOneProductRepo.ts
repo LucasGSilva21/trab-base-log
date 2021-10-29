dd18e40ea65d2da6213212c37283af03f769965c
Lucas Gabriel <lucasgsilva2102@gmail.com>
2021-10-29 18:58:28 -0300

    refactor: move not found validation in get one product

--- src/infra/databases/postgres/repositories/product/GetOneProductRepo.ts
+++ src/infra/databases/postgres/repositories/product/GetOneProductRepo.ts
@@ -3,7 +3,7 @@ import { GetOneProductRepository } from '../../../../../domain/repositories/prod
 import { PgRepository } from '../../helpers/repository'
 import { PgProduct } from '../../entities'
 import { UUIDValidator } from '../../../../../utils/validators/protocols'
-import { InvalidParamError } from '../../../../../utils/errors'
+import { InvalidParamError, NotFoundError } from '../../../../../utils/errors'
 
 export class PgGetOneProductRepository extends PgRepository implements GetOneProductRepository {
   private readonly uUIDValidator: UUIDValidator
@@ -24,6 +24,10 @@ export class PgGetOneProductRepository extends PgRepository implements GetOnePro
     const pgProductRepo = this.getRepository(PgProduct)
 
     const product = await pgProductRepo.findOne(id)
+
+    if (!product) {
+      throw new NotFoundError(id)
+    }
 
     return product
   }
--- src/presentation/controllers/product/GetOneProduct.ts
+++ src/presentation/controllers/product/GetOneProduct.ts
@@ -2,7 +2,6 @@ import { HttpRequest, HttpResponse, Controller } from '../../protocols'
 import { GetOneProduct } from '../../../domain/usecases/product'
 import { ok } from '../../helpers/http-helper'
 import { validateResponseError } from '../../helpers/validate-response-error'
-import { NotFoundError } from '../../../utils/errors'
 
 export class GetOneProductController implements Controller {
   private readonly getOneProduct: GetOneProduct
@@ -16,10 +15,6 @@ export class GetOneProductController implements Controller {
       const { id } = httpRequest.params
 
       const product = await this.getOneProduct.getOne(id)
-
-      if (!product) {
-        throw new NotFoundError(id)
-      }
 
       return ok(product)
     } catch (error) {
