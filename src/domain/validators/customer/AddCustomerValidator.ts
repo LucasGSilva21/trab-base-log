68243cd38c3aef2c136c4e2f304949b0866a1d17
Lucas Gabriel <lucasgsilva2102@gmail.com>
2021-10-30 10:32:52 -0300

    feat: add validate for add customer

--- src/domain/validators/customer/AddCustomerValidator.ts
+++ src/domain/validators/customer/AddCustomerValidator.ts
@@ -1,4 +1,4 @@
-import { Validator } from '../../protocols'
+import { Validator, Gender } from '../../protocols'
 import { AddCustomerModel } from '../../repositories/customer'
 import { MissingParamError, InvalidParamError } from '../../../utils/errors'
 import { EmailValidator } from '../../../utils/validators/protocols'
@@ -19,6 +19,20 @@ export class AddCustomerValidator implements Validator {
       if (addCustomerModel[field] === undefined) {
         throw new MissingParamError(field)
       }
+    }
+
+    if (isNaN(Date.parse(addCustomerModel.birthDate.toString()))) {
+      throw new InvalidParamError(
+        'birthDate',
+        'invalid birth date'
+      )
+    }
+
+    if (!(addCustomerModel.gender in Gender)) {
+      throw new InvalidParamError(
+        'gender',
+        'invalid gender value'
+      )
     }
 
     if (!this.emailValidator.isValid(addCustomerModel.email)) {
