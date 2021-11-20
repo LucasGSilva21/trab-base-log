a324716604f3f7ec3c3dcbc5636be35b05acb257
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2021-11-20 11:16:41 -0300

    test: ensure not create a product with invalid values

--- tests/main/routes/product/add-product.test.ts
+++ tests/main/routes/product/add-product.test.ts
@@ -36,4 +36,144 @@ describe('SignUp Routes', () => {
       })
       .expect(200)
   })
+
+  test('Should return a missing param error if not send name field', async () => {
+    const { body, statusCode } = await request(app)
+      .post('/api/products')
+      .send({
+        description: 'test',
+        purchasePrice: 30,
+        salePrice: 40,
+        quantity: 10
+      })
+
+    expect(statusCode).toBe(400)
+    expect(body).toEqual({
+      name: 'MissingParamError',
+      message: 'name is required'
+    })
+  })
+
+  test('Should return a missing param error if not send purchase price field', async () => {
+    const { body, statusCode } = await request(app)
+      .post('/api/products')
+      .send({
+        name: 'test',
+        description: 'test',
+        salePrice: 40,
+        quantity: 10
+      })
+
+    expect(statusCode).toBe(400)
+    expect(body).toEqual({
+      name: 'MissingParamError',
+      message: 'purchasePrice is required'
+    })
+  })
+
+  test('Should return a missing param error if not send sale price field', async () => {
+    const { body, statusCode } = await request(app)
+      .post('/api/products')
+      .send({
+        name: 'test',
+        description: 'test',
+        purchasePrice: 30,
+        quantity: 10
+      })
+
+    expect(statusCode).toBe(400)
+    expect(body).toEqual({
+      name: 'MissingParamError',
+      message: 'salePrice is required'
+    })
+  })
+
+  test('Should return a missing param error if not send quantity field', async () => {
+    const { body, statusCode } = await request(app)
+      .post('/api/products')
+      .send({
+        name: 'test',
+        description: 'test',
+        purchasePrice: 30,
+        salePrice: 40
+      })
+
+    expect(statusCode).toBe(400)
+    expect(body).toEqual({
+      name: 'MissingParamError',
+      message: 'quantity is required'
+    })
+  })
+
+  test('Should return a invalid param error if purchase price field is not a number', async () => {
+    const { body, statusCode } = await request(app)
+      .post('/api/products')
+      .send({
+        name: 'test',
+        description: 'test',
+        purchasePrice: 'invalid',
+        salePrice: 40,
+        quantity: 10
+      })
+
+    expect(statusCode).toBe(400)
+    expect(body).toEqual({
+      name: 'InvalidParamError',
+      message: 'Purchase price must be a number'
+    })
+  })
+
+  test('Should return a invalid param error if sale price field is not a number', async () => {
+    const { body, statusCode } = await request(app)
+      .post('/api/products')
+      .send({
+        name: 'test',
+        description: 'test',
+        purchasePrice: 30,
+        salePrice: 'invalid',
+        quantity: 10
+      })
+
+    expect(statusCode).toBe(400)
+    expect(body).toEqual({
+      name: 'InvalidParamError',
+      message: 'Sale price must be a number'
+    })
+  })
+
+  test('Should return a invalid param error if quantity field is not a positive integer', async () => {
+    const { body, statusCode } = await request(app)
+      .post('/api/products')
+      .send({
+        name: 'test',
+        description: 'test',
+        purchasePrice: 30,
+        salePrice: 40,
+        quantity: -10
+      })
+
+    expect(statusCode).toBe(400)
+    expect(body).toEqual({
+      name: 'InvalidParamError',
+      message: 'The quantity must be a positive integer'
+    })
+  })
+
+  test('Should return a invalid param error if purchase price more than sale price', async () => {
+    const { body, statusCode } = await request(app)
+      .post('/api/products')
+      .send({
+        name: 'test',
+        description: 'test',
+        purchasePrice: 40,
+        salePrice: 30,
+        quantity: 10
+      })
+
+    expect(statusCode).toBe(400)
+    expect(body).toEqual({
+      name: 'InvalidParamError',
+      message: 'Sale price must be greater than to purchase price'
+    })
+  })
 })
