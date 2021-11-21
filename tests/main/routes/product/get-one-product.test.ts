3d30f37657bce3b89cfbd7541524e534cde29f3a
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2021-11-21 09:43:04 -0300

    test: ensure return a product on success

---
+++ tests/main/routes/product/get-one-product.test.ts
@@ -0,0 +1,42 @@
+import request from 'supertest'
+import { app } from '../../../../src/main/config/app'
+import { Repository } from 'typeorm'
+import { PgConnection } from '../../../../src/infra/databases/postgres/helpers/connection'
+import { PgProduct } from '../../../../src/infra/databases/postgres/entities'
+
+describe('Get One Product Route', () => {
+  let connection: PgConnection
+  let pgProductRepo: Repository<PgProduct>
+
+  beforeAll(async () => {
+    connection = PgConnection.getInstance()
+    await connection.connect()
+    await connection.runMigrations()
+    pgProductRepo = connection.getRepository(PgProduct)
+  })
+
+  afterAll(async () => {
+    await pgProductRepo.delete({})
+    await connection.disconnect()
+  })
+
+  beforeEach(async () => {
+    await pgProductRepo.delete({})
+  })
+
+  test('Should return a product on success', async () => {
+    const { id } = await pgProductRepo.save({
+      name: 'test1',
+      description: 'test1',
+      purchasePrice: 30,
+      salePrice: 40,
+      quantity: 10
+    })
+
+    const { body, statusCode } = await request(app)
+      .get(`/api/products/${id}`)
+
+    expect(statusCode).toBe(200)
+    expect(body.id).toBe(id)
+  })
+})
