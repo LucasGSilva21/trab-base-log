cebe0ffa44e6ee8d8380b786b470f34c3e00f73c
Rafael Nogueira Rabelo <rafaelnrabelo@gmail.com>
2021-11-21 11:12:22 -0300

    test: ensure delete a customer on success

---
+++ tests/main/routes/customer/delete-customer.test.ts
@@ -0,0 +1,44 @@
+import request from 'supertest'
+import { app } from '../../../../src/main/config/app'
+import { Repository } from 'typeorm'
+import { PgConnection } from '../../../../src/infra/databases/postgres/helpers/connection'
+import { PgCustomer } from '../../../../src/infra/databases/postgres/entities'
+import { Gender } from '../../../../src/domain/protocols/enums'
+
+describe('Delete Customer Route', () => {
+  let connection: PgConnection
+  let PgCustomerRepo: Repository<PgCustomer>
+
+  beforeAll(async () => {
+    connection = PgConnection.getInstance()
+    await connection.connect()
+    await connection.runMigrations()
+    PgCustomerRepo = connection.getRepository(PgCustomer)
+  })
+
+  afterAll(async () => {
+    await PgCustomerRepo.delete({})
+    await connection.disconnect()
+  })
+
+  beforeEach(async () => {
+    await PgCustomerRepo.delete({})
+  })
+
+  test('Should delete a customer on success', async () => {
+    const { id } = await PgCustomerRepo.save({
+      name: 'test',
+      email: 'test@test.com',
+      cpf: '111.111.111-11',
+      birthDate: new Date(),
+      gender: Gender.MALE,
+      phone: '(11) 99999-9999',
+      address: 'test'
+    })
+
+    const { statusCode } = await request(app)
+      .delete(`/api/customers/${id}`)
+
+    expect(statusCode).toBe(204)
+  })
+})
